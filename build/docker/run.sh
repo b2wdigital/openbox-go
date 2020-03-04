ae6174fded1f23bf3de9fd01e724a3f1ba0e0b72
João Paulo Faria <jpfaria@gmail.com>
2020-03-03 21:51:50 -0300

    Initial commit

---
+++ build/docker/run.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "                         _                "
+echo "                        | |               "
+echo "   ___  _ __   ___ _ __ | |__   _____  __ "
+echo "  / _ \| '_ \ / _ \ '_ \| '_ \ / _ \ \/ / "
+echo " | (_) | |_) |  __/ | | | |_) | (_) >  <  "
+echo "  \___/| .__/ \___|_| |_|_.__/ \___/_/\_\ "
+echo "       | |                                "
+echo "       |_|                                "
+echo "                                          "
+echo "B2W Digital                               "
+echo "http://github.com/b2wdigital/openbox-go   "
+echo "                                          "
+echo "Application Arguments:                    "
+echo $GO_ARGS
+echo "                                          "
+echo "                                          "
+
+/usr/local/bin/app $GO_ARGS
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/b2wdigital/openbox-go
+
+go 1.13
+
+require (
+	github.com/b2wdigital/goignite v0.0.12
+	github.com/cloudevents/sdk-go v1.0.0
+)
